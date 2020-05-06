2bb04e278a62586d9284eb9e3e57bec23b85b77e
Anath <anathkantonda@gmail.com>
2020-05-06 19:16:56 +0200

    Updates

---
+++ precache-manifest.60bac625d86775df349bb745a3105a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df992dce895a4fa2a1ae16ad2d6e9dad",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "52f190e6e3333e863b71",
+    "url": "/recipes/static/css/main.a13e5954.chunk.css"
+  },
+  {
+    "revision": "7c04d74550259d40ba07",
+    "url": "/recipes/static/js/2.d21889c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.d21889c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f190e6e3333e863b71",
+    "url": "/recipes/static/js/main.8f60a7af.chunk.js"
+  },
+  {
+    "revision": "398ad8e94458d47aae40",
+    "url": "/recipes/static/js/runtime-main.f63ee6ac.js"
+  }
+]);
